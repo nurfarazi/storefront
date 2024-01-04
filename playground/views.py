72793383ba7986ecc6180e41ab336439c5dc538c
nur farazi <nur369188@gmail.com>
2024-01-04 23:09:41 +0600

    Add django-debug-toolbar to Pipfile and Pipfile.lock, and include debug_toolbar in INSTALLED_APPS and urlpatterns

--- playground/views.py
+++ playground/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    x = 1
     return HttpResponse("Hello, world. You're at the playground index.")
 def get_html(request):
     return render(request, 'index.html')
